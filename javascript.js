f426d5118d421602100df007ced169e19d0b634c
Abdelrhman <264638508+theabdelrhmanahmed@users.noreply.github.com>
2026-07-29 17:50:06 +0300

    comment out some comments

--- javascript.js
+++ javascript.js
@@ -1,11 +1,4 @@
 'use strict';
-/*
-Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-Hint: The Math.random method returns a random number that’s greater than or equal to 0 and less than 1.
- Think about how you can use this to conditionally return one of the multiple choices.
-Note: Some learners might think that arrays are necessary for this problem, but they aren’t.
- If you’re unfamiliar with arrays, don’t worry - they’ll be covered later in the curriculum.
-*/
 const getComputerChoice =function(){
     const comChoice =Math.trunc(Math.random()*3)+1;
     switch (comChoice){
